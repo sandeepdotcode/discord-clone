af9f46b12bbfc1a818e7acae61ef5cb85f0a0b29
Sandeep K K <sandeepdotcode@gmail.com>
2023-09-27 20:17:39 +0530

    Setup dark mode and create dark mode toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import LogoutBtn from '@/components/auth/LogoutBtn';
+import { ModeToggle } from '@/components/ui/mode-toggle';
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <div className="text-3xl font-bold text-indigo-500">Hello World</div>
       <div>This is a protected route</div>
       <LogoutBtn />
+      <ModeToggle />
     </div>
   )
 }
