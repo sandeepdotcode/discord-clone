dbd6d03f86c0d95ccd38f45351b9317bcf3badee
Sandeep K K <sandeepdotcode@gmail.com>
2023-10-27 22:41:38 +0530

    Beutify home page

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,10 +2,12 @@ import LogoutBtn from '@/components/auth/LogoutBtn';
 import { ModeToggle } from '@/components/ui/mode-toggle';
 import { db } from '@/lib/db/db';
 import { eq } from 'drizzle-orm';
-import { members } from '@/lib/db/schema';
+import { members, users } from '@/lib/db/schema';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import InitialModal from '@/components/modals/InitialModal';
+import Image from 'next/image';
+import { getInitials } from '@/lib/utils';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -14,18 +16,36 @@ export default async function Home() {
     data: { user }
   } = await supabase.auth.getUser();
 
+  const userData = await db.query.users.findFirst({
+    where: eq(users.id, user?.id),
+  });
+  console.log(userData);
+
   const memberShip = await db
     .select()
     .from(members)
     .where(eq(members.userId, user?.id));
 
   return (
-    <div className="flex flex-col items-start">
-      <div className="text-3xl font-bold text-indigo-500">Hello World</div>
-      <div>This is a protected route</div>
-      <LogoutBtn />
-      <ModeToggle />
+    <div className="flex flex-col items-start h-full">
+      <div className="h-8 w-full flex justify-end gap-4 p-2">
+        <ModeToggle />
+        <LogoutBtn />
+      </div>
+      <div className="h-full w-full flex flex-col items-center justify-center space-y-4">
+        <div className="h-[88px] w-[88px] rounded-md overflow-hidden relative">
+          { userData?.avatarUrl
+            ? <Image fill src={userData.avatarUrl} alt='' />
+            : <div className="h-full w-full flex justify-center items-center dark:bg-zinc-600 text-3xl">
+                { getInitials(userData?.displayName ? userData.displayName : userData?.username) }
+              </div>
+          }
+        </div>
+        <div className='text-3xl font-bold dark:text-primary/80'>Hello, { userData?.displayName ? userData.displayName : userData?.username }</div>
+        <div className="text-2xl font-bold dark:text-primary/80">Welcome to <span className="text-pink-600">Moocord</span>!</div>
+      </div>
       { !memberShip.length && <InitialModal /> }
+      <div className="flex justify-end w-full p-4 text-sm text-bold dark:text-primary/70">This is a protected route!</div>
     </div>
   )
 }
