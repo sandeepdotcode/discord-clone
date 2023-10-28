212809bc300e6142e8f24b7e2ef504c0ad9180f0
Sandeep K K <sandeepdotcode@gmail.com>
2023-10-28 15:21:52 +0530

    Fix hmr creating multiple drizzle clients in dev server

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,10 +1,27 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import postgres from 'postgres';
- 
+import { global } from 'styled-jsx/css';
+
+let db: PostgresJsDatabase<typeof schema>;
+
+declare global {
+	var __db__: PostgresJsDatabase<typeof schema>;
+}
+
 const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
-const db = drizzle(client, { schema });
-console.log('drizzle client created');
+
+if (process.env.NODE_ENV === "production") {
+	const client = postgres(connectionString);
+	db = drizzle(client, { schema });
+	console.log('drizzle client created');
+} else {
+	if (!global.__db__) {
+		global.__db__ = drizzle(postgres(connectionString), {
+			schema,
+		});
+	}
+	db = global.__db__;
+}
 
 export { db };
